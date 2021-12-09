e12b7911b0a09070b34a7d738012b52bd5f99bc9
wangyibo <2375847849@qq.com>
2021-12-09 23:44:35 +0800

    Update LoginController.java

--- springboot-03-web2/src/main/java/com/wang/controller/LoginController.java
+++ springboot-03-web2/src/main/java/com/wang/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
+                         
 @Controller
 public class LoginController {
 
