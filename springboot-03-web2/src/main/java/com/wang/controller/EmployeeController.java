32560eaef62e1dbf093cab1279130d55e56762d6
wangyibo <2375847849@qq.com>
2021-12-11 23:19:48 +0800

    Update EmployeeController.java

--- springboot-03-web2/src/main/java/com/wang/controller/EmployeeController.java
+++ springboot-03-web2/src/main/java/com/wang/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-
+                 
 @Controller
 public class EmployeeController {
 
